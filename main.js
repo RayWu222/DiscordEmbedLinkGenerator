115f678f9bb02341419558bb56059a6f21079793
RayWu222 <48284351+RayWu222@users.noreply.github.com>
2023-10-31 16:53:09 -0700

    Add clear function

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const button  = document.querySelector('#submit');
 const clear = document.querySelector('#clear');
-const fixv = "https://fixv";
 const input = document.querySelector('input');
 const box = document.querySelector('#result');
+const fixv = "https://fixv";
 let urlList = "";
+
+
 button.addEventListener('click', () =>{
-    
     let domain = (new URL(input.value));
     let hostname = domain.hostname;
     let path = domain.pathname;
     if(hostname == 'x.com'){
         urlList += fixv + hostname + path + "\n" ;
-        box.textContent =  urlList;
-        
+        box.textContent =  urlList;     
     }
+    input.value = "";
 });
+
+clear.addEventListener('click', ()=>{
+    box.textContent = "";
+})

37807be15744651247067f7faa3357acd5152d6f
RayWu222 <48284351+RayWu222@users.noreply.github.com>
2023-10-31 16:50:01 -0700

    add clear button

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const button  = document.querySelector('#submit');
+const clear = document.querySelector('#clear');
 const fixv = "https://fixv";
 const input = document.querySelector('input');
 const box = document.querySelector('#result');
+let urlList = "";
 button.addEventListener('click', () =>{
     
     let domain = (new URL(input.value));
     let hostname = domain.hostname;
     let path = domain.pathname;
     if(hostname == 'x.com'){
-        hostname = fixv + hostname + path;
+        urlList += fixv + hostname + path + "\n" ;
+        box.textContent =  urlList;
         
     }
 });
